2e364c4d1570e090e9a3a170f66772f30df373c7
senghongoo <sehong29@gmail.com>
2025-04-24 18:54:12 +0900

    2025-04-24

--- js/main.js
+++ js/main.js
@@ -1,10 +1,8 @@
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 6,
     spaceBetween: 20,
-    loop: true,
     grabCursor: true,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    }
+    scrollbar: {
+        el: ".swiper-scrollbar",
+      },
 });
